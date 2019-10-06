d608f04cbc92297c67b231027ab55830749f8597
Nikita Bukin <locus2@mail.ru>
2019-10-06 16:37:33 +0300

    add func getGift

---
+++ Part6/src/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import toggleChooseClub from './modules/toggleChooseClub';
+import getPopups from './modules/getPopups';
+import sendForm from './modules/sendForm';
+import validator from './modules/validator';
+import correctInput from './modules/correctInput';
+import getGift from './modules/getGift';
+
+toggleChooseClub();
+getPopups();
+sendForm();
+validator();
+correctInput();
+getGift();
