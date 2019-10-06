2d5dc89eca92f9f1a60302e58092878421e34cba
Nikita Bukin <locus2@mail.ru>
2019-10-06 15:27:56 +0300

    add part 5

---
+++ Part5/src/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import toggleChooseClub from './modules/toggleChooseClub';
+import getPopups from './modules/getPopups';
+import sendForm from './modules/sendForm';
+import validator from './modules/validator';
+import correctInput from './modules/correctInput';
+
+toggleChooseClub();
+getPopups();
+sendForm();
+validator();
+correctInput();
