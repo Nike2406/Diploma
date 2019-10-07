392813c712abfb03f9f01324a3a4a9ffca10c530
Nikita Bukin <locus2@mail.ru>
2019-10-08 01:39:16 +0300

    add thanks-popup

---
+++ Part8/src/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import toggleChooseClub from './modules/toggleChooseClub';
+import getPopups from './modules/getPopups';
+import sendForm from './modules/sendForm';
+import validator from './modules/validator';
+import correctInput from './modules/correctInput';
+import getGift from './modules/getGift';
+import servicesSlider from './modules/servicesSlider';
+import headSlider from './modules/headSlider';
+import gellarySlider from './modules/gellarySlider';
+
+toggleChooseClub();
+getPopups();
+sendForm();
+validator();
+correctInput();
+getGift();
+servicesSlider();
+headSlider();
+gellarySlider();
