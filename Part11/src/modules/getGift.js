b8cee2b520ef1155651b1ec539b6e132e2f34c6a
Nikita Bukin <locus2@mail.ru>
2019-10-08 20:27:11 +0300

    add clubCalculate

---
+++ Part11/src/modules/getGift.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const getGift = () => {
+
+    const fixedGift = document.querySelector('.fixed-gift'),
+        gift = document.getElementById('gift');
+    fixedGift.addEventListener('click', () => {
+        gift.style.display = 'block';
+        fixedGift.remove();
+    });
+    gift.addEventListener('click', (e) => {
+        let target = e.target;
+        if (!target.closest('.form-content') || target.classList.contains('close-btn')) {
+            gift.style.display = 'none';
+        }
+    });
+};
+
+export default getGift;
