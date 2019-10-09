f2757003da4577b08c73fdb45ed4068ea0e831b6
Nikita Bukin <locus2@mail.ru>
2019-10-09 05:04:44 +0300

    add common fix, fix multiple pages

---
+++ Part13/src/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import toggleChooseClub from './modules/toggleChooseClub';
+import getPopups from './modules/getPopups';
+import sendForm from './modules/sendForm';
+import validator from './modules/validator';
+import correctInput from './modules/correctInput';
+import getGift from './modules/getGift';
+import servicesSlider from './modules/servicesSlider';
+import headSlider from './modules/headSlider';
+import gellarySlider from './modules/gellarySlider';
+import priceCalculate from './modules/priceCalculate';
+
+toggleChooseClub();
+getPopups();
+sendForm();
+validator();
+correctInput();
+getGift();
+servicesSlider();
+headSlider();
+gellarySlider();
+priceCalculate();
